e4506391c1a8a26fb917d0964c32183d945f900f
Gabriel Santos <gabr1el_santos@hotmail.com>
2020-01-18 12:20:27 -0300

    Criado a rota de likes e dislikes

---
+++ src/Controller/Like.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-underscore-dangle */
+const Dev = require(`../models/Dev`);
+
+const store = async (req, res) => {
+  try {
+    const { user } = req.headers;
+    const { devId: id } = req.params;
+
+    const logged = await Dev.findById(user);
+    const target = await Dev.findById(id);
+
+    if (!target) {
+      return res.status(404).json({
+        error: "User not found"
+      });
+    }
+    logged.likes.push(target._id);
+    await logged.save();
+
+    res.json(logged);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
+};
+
+module.exports = { store };
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 const DevController = require("./Controller/Dev");
 
+const LikeController = require(`./Controller/Like`);
+const DislikeController = require(`./Controller/Dislike`);
+
 router.get("/devs", DevController.index);
 router.post(`/devs`, DevController.store);
+
+router.post("/devs/:devId/likes", LikeController.store);
+router.post("/devs/:devId/dislike", DislikeController.store);
 
 module.exports = router;
